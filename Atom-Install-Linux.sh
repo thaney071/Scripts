ae23b8f31210ed18f6c8ad536234632a50e8157d
Thomas Haney <thomas_haney@selinc.com>
2016-09-02 10:52:12 -0700

    Refactor installer

--- Atom-Install-Linux.sh
+++ Atom-Install-Linux.sh
@@ -1,18 +1,59 @@
+# @File: Atom-Install-Linux.sh
+# @Author: Thomas Haney
+# @Date:   2016-09-02T09:31:21-07:00
+# @Email:  thaney071@gmail.com
+# @Last modified by:   Thomas Haney
+# @Last modified time: 2016-09-02T10:31:08-07:00
+# @Description: An installer for Atom text editor on Linux.
+
 #!/bin/bash
 
+Define variables:
+txtund=$(tput sgr 0 1)    # Underline
+txtbld=$(tput bold)       # Bold
+txtred=$(tput setaf 1)    # Red
+txtgrn=$(tput setaf 2)    # Green
+txtylw=$(tput setaf 3)    # Yellow
+txtblu=$(tput setaf 4)    # Blue
+txtpur=$(tput setaf 5)    # Purple
+txtcyn=$(tput setaf 6)    # Cyan
+txtwht=$(tput setaf 7)    # White
+txtrst=$(tput sgr0)       # Text reset
+
+format="\n%-10s ${txtbld}%-30s ${txtrst} %10s\n\n"
+f_done="${txtgrn}%20s${txtrst}\n"
+f_error="${txtred}%20s${txtrst}\n"
+f_stop="${txtred}%s${txtrst}\n"
+work="\n${txtylw}%-40s${txtrst}"
 clear
 
 # If  su, this script will rerror and exit
 if [ "$(id -u)" == '0' ] ; then
-   printf "%s\n" "Atom-Install must be run as local user (run again without sudo)"
+   printf "$f_error" "Atom-Install must be run as local user (run again without sudo)"
    exit $?
 fi
+printf "$format" "----" "Downloading Atom deb package" "----"
 
 sudo wget -q https://github.com/atom/atom/releases/latest -O /tmp/latest
 sudo wget --progress=bar -q 'https://github.com'$(cat /tmp/latest | grep -o -E 'href="([^"#]+)atom-amd64.deb"' | cut -d'"' -f2 | sort | uniq) -O /tmp/atom-amd64.deb -q --show-progress
+
+printf "$format" "----" "Installing Atom" "----"
 sudo dpkg -i /tmp/atom-amd64.deb
 
-printf "\n----Intalling Additonal Atom Packages----\n\n"
-apm install --packages-files atom-package-list.txt
+printf "$format" "----" "Intalling Additonal Atom Packages" "----"
 
-chown -Rv  $USER.$USER  ~/.atom
+sudo apm install --packages-file atom-package-list.txt
+
+printf "$format" "----" "Setting Atom Config" "----"
+
+printf "$work" "Atom Config:"
+mv ~/.atom/config.cson ~/.atom/config-default.cson
+cp -p config.cson ~/.atom
+if [[ $? -ne 0 ]]; then
+  printf "$f_error" "Failed - Error placeing config"
+  printf "$f_stop" "Stopping"
+  exit 1
+else
+  printf "$f_done" "DONE"
+fi
+
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Scripts
 Helper scripts to automate tasks
+
+## Atom Installer Linux
+This script is used as an installer for Atom text editor.
+
+1. Download the latest Atom deb from the Atom web page.
+2. dpkg the downloaded deb
+3. Use apm to install additional Atom packages from atom-package-list.txt
+4. Make a backup of the config.cson file
+5. Place a new config.cson file.
+
+For now, to change the additional Atom packages, you can replace the
+atom-package-list.txt file with your own list of atom packages.
+
+If you want to return to the Default atom config, replace the config.cson file
+with the config-default.cson. The config-default.cson is the original Atom
+settings.
